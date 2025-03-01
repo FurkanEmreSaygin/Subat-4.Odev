be5aca0a878e0e04eff155ee53283437644d1c73
Furkan Emre Saygin <furkanemresaygin@gmail.com>
2025-03-01 17:19:18 +0300

    3 Sec Problem

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program{
+    
+    public static async Task LongTermOperating(){
+
+        Console.WriteLine("3 second countdown started");
+        await Task.Delay(3000);
+        Console.WriteLine("3 second is done !!!");
+    
+    }
+    
+    
+    static async Task Main(){
+        
+        Console.WriteLine("Program Starting");
+        
+        await LongTermOperating();
+        
+        Console.WriteLine("Program Ended");
+
+    }
+
+}
